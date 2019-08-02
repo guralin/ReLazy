742576ecb512ee7e70711c3bc11ccbaf135db96d
K1-scuba_diving <tellurium38@gmail.com>
2019-08-02 23:43:19 +0900

    add_timestamp_for_database

--- create_db.py
+++ create_db.py
@@ -5,7 +5,11 @@ import sqlite3
 
 conn = sqlite3.connect("word.db")
 
+# todo:Unixタイムで保存するか、文字列型で保存するか迷っている
+# この世界線では文字列で実装してみる
+
 sql = """CREATE TABLE words(id integer primary key,
+timestamp text,
 jpn_word text,
 eng_word text
 );
