2e207c862f10c9d6030edf7f3ba2c722090fdbe5
guralin <tellurium38@gmail.com>
2019-08-15 01:33:35 +0900

    最後にエイリアスを読み込ませるよう変更

--- set_command.sh
+++ set_command.sh
@@ -3,4 +3,7 @@ is_relazy_command=`cat ~/.bashrc | grep "alias relazy" | wc -l`
 if [[ $is_relazy_command -eq 0 ]]; then
     relazy_path=`pwd`
     echo "alias relazy='python ${relazy_path}/scraping.py'" >> ~/.bashrc
+    source ~/.bashrc
+else
+    echo "設定済み"
 fi
