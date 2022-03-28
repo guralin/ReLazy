5cdff742d1263e36c1302c67b8f7941b8426a81d
guralin <tellurium38@gmail.com>
2022-03-29 08:40:32 +0900

    DeepLを使用するよう修正

--- set_command.sh
+++ set_command.sh
@@ -2,8 +2,19 @@
 is_relazy_command=`cat ~/.bashrc | grep "alias relazy" | wc -l`
 if [[ $is_relazy_command -eq 0 ]]; then
     relazy_path=`pwd`
-    echo "alias relazy='python ${relazy_path}/scraping.py'" >> ~/.bashrc
+    echo "alias relazy='python ${relazy_path}/main.py'" >> ~/.bashrc
     source ~/.bashrc
 else
-    echo "設定済み"
+    echo "relazy_pathは設定済み"
+fi
+
+count_deepl_environment=`cat ~/.bashrc | grep "export DEEPL_AUTH_KEY" | wc -l`
+
+if [[ $count_depl_environment -eq 0 ]]; then
+    echo "DEEPL_AUTH_KEYを入力してください＞"
+    read AUTH_KEY
+    echo "export DEEPL_AUTH_KEY=$AUTH_KEY" >> ~/.bashrc
+    source ~/.bashrc
+else
+    echo "AUTH_KEYは設定済み"
 fi
